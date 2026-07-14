09dd1b85c76b5e8b7e26f4a8167c3cf91082cecb
juliajulha54-prog <juliajulha54@gmail.com>
2026-07-13 21:56:33 -0300

    Update verification.py

--- cogs/verification.py
+++ cogs/verification.py
@@ -7,24 +7,35 @@ INVITES_FILE = "invites.json"
 
 ROLE_ID = 1524804424352927845
 INVITE_CHANNEL_ID = 1526281178934542337
+REQUIRED_INVITES = 3
 
 
 class Verification(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.invites = {}
+        self.invites_cache: dict[int, list[discord.Invite]] = {}
         self.data = self.load_data()
 
     def load_data(self):
         if not os.path.exists(INVITES_FILE):
             with open(INVITES_FILE, "w", encoding="utf-8") as f:
-                json.dump({}, f, indent=4)
+                json.dump(
+                    {
+                        "users": {},
+                        "members": {}
+                    },
+                    f,
+                    indent=4
+                )
 
         with open(INVITES_FILE, "r", encoding="utf-8") as f:
             data = json.load(f)
 
-        if not isinstance(data, dict):
-            data = {}
+        if "users" not in data:
+            data["users"] = {}
+
+        if "members" not in data:
+            data["members"] = {}
 
         return data
 
@@ -32,119 +43,65 @@ class Verification(commands.Cog):
         with open(INVITES_FILE, "w", encoding="utf-8") as f:
             json.dump(self.data, f, indent=4)
 
-    async def cache_invites(self):
-        self.invites.clear()
-
-        for guild in self.bot.guilds:
-            try:
-                self.invites[guild.id] = await guild.invites()
-            except discord.Forbidden:
-                pass
-
     def ensure_user(self, user_id: int):
         uid = str(user_id)
 
-        if uid not in self.data:
-            self.data[uid] = {
-                "invited": []
+        if uid not in self.data["users"]:
+            self.data["users"][uid] = {
+                "invited": [],
+                "verified": False
             }
 
-    def add_invited_member(self, inviter_id: int, member_id: int):
+    def get_user(self, user_id: int):
+        self.ensure_user(user_id)
+        return self.data["users"][str(user_id)]
+
+    def get_invites(self, user_id: int):
+        return len(self.get_user(user_id)["invited"])
+
+    def is_verified(self, user_id: int):
+        return self.get_user(user_id)["verified"]
+
+    def set_verified(self, user_id: int):
+        self.ensure_user(user_id)
+        self.data["users"][str(user_id)]["verified"] = True
+        self.save_data()
+
+    def add_invite(self, inviter_id: int, member_id: int):
         self.ensure_user(inviter_id)
 
-        invited = self.data[str(inviter_id)]["invited"]
+        invited = self.data["users"][str(inviter_id)]["invited"]
 
         if member_id not in invited:
             invited.append(member_id)
-            self.save_data()
 
-    def remove_invited_member(self, inviter_id: int, member_id: int):
-        self.ensure_user(inviter_id)
+        self.data["members"][str(member_id)] = inviter_id
 
-        invited = self.data[str(inviter_id)]["invited"]
+        self.save_data()
 
-        if member_id in invited:
-            invited.remove(member_id)
-            self.save_data()
+    def remove_invite(self, member_id: int):
+        mid = str(member_id)
 
-    def get_invites(self, user_id: int):
-        self.ensure_user(user_id)
-        return len(self.data[str(user_id)]["invited"])
-
-    def get_invited_members(self, user_id: int):
-        self.ensure_user(user_id)
-        return self.data[str(user_id)]["invited"]
-
-    def find_inviter(self, member_id: int):
-        for inviter_id, info in self.data.items():
-            if member_id in info.get("invited", []):
-                return int(inviter_id)
-        return None
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        await self.cache_invites()
-
-    @commands.Cog.listener()
-    async def on_invite_create(self, invite: discord.Invite):
-        try:
-            self.invites[invite.guild.id] = await invite.guild.invites()
-        except discord.Forbidden:
-            pass
-
-    @commands.Cog.listener()
-    async def on_invite_delete(self, invite: discord.Invite):
-        try:
-            self.invites[invite.guild.id] = await invite.guild.invites()
-        except discord.Forbidden:
-            pass
-
-    @commands.Cog.listener()
-    async def on_member_join(self, member: discord.Member):
-        if member.bot:
+        if mid not in self.data["members"]:
             return
 
-        guild = member.guild
+        inviter_id = str(self.data["members"][mid])
 
-        try:
-            current_invites = await guild.invites()
-        except discord.Forbidden:
-            return
+        if inviter_id in self.data["users"]:
+            invited = self.data["users"][inviter_id]["invited"]
 
-        previous_invites = self.invites.get(guild.id, [])
+            if member_id in invited:
+                invited.remove(member_id)
 
-        used_invite = None
+        del self.data["members"][mid]
 
-        for current in current_invites:
-            old = discord.utils.get(previous_invites, code=current.code)
+        self.save_data()
 
-            if old and current.uses > old.uses:
-                used_invite = current
-                break
+    async def cache_invites(self):
+        self.invites_cache.clear()
 
-        self.invites[guild.id] = current_invites
-
-        if used_invite is None:
-            return
-
-        inviter = used_invite.inviter
-
-        if inviter is None or inviter.bot:
-            return
-
-        if inviter.id == member.id:
-            return
-
-        self.add_invited_member(inviter.id, member.id)
-
-    @commands.Cog.listener()
-    async def on_member_remove(self, member: discord.Member):
-        inviter_id = self.find_inviter(member.id)
-
-        if inviter_id:
-            self.remove_invited_member(inviter_id, member.id)
-
-        try:
-            self.invites[member.guild.id] = await member.guild.invites()
-        except discord.Forbidden:
-            pass
+        for guild in self.bot.guilds:
+            try:
+                self.invites_cache[guild.id] = await guild.invites()
+            except discord.Forbidden:
+                pass
